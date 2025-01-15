3479719431a53244baec15e530c80c53e0e2f71b
Andry <andry.carolina88@gmail.com>
2025-01-15 14:41:06 -0500

    16 test pass

--- js/user/user-controller.test.js
+++ js/user/user-controller.test.js
@@ -22,6 +22,30 @@ test('add new user', () => {
   expect(userController.users).toContain(user)
 })
 
-test('remove user', () => {
-  
+test('remove user', () => {    
+  let user = new User(12345,"Santi", "s@generation.org");
+  userController.add(user);    
+  userController.remove(user);
+  expect(userController.users).not.toContain(user);
+});
+
+test('findbyemail true', () => {
+  const user = new User(2, 'Jose', 'j@org.com')
+  userController.add(user)
+  expect(userController.findByEmail('j@org.com')).toBe(user)
+  // expect(userController.findByEmail('j@org.com')).toMatch(user)
+})
+
+test('findbyemail false', () => {  
+  expect(userController.findByEmail('joselito@org.com')).toBe(undefined)
+})
+
+test('finbyId true', () => {
+  const user = new User(3, 'Josefina', 'jp@org.com')
+  userController.add(user)
+  expect(userController.findById(3)).toBe(user)
+})
+
+test('findbyeId false', () => {  
+  expect(userController.findById(20)).toBe(undefined)
 })
